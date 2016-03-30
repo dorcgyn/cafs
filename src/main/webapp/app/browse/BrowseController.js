065dfffe25ab9144a17711a1f5e7ee5660c55cc7
dev <dev@cmxdev.americas.hpqcorp.net>
2016-03-30 10:27:31 -0400

    css and preload in web

--- src/main/webapp/app/browse/BrowseController.js
+++ src/main/webapp/app/browse/BrowseController.js
@@ -7,27 +7,28 @@
 var app = angular.module('app', []);
 
 
-app.controller("BrowseCtrl", function(BrowseService, $scope)
+app.controller("BrowseCtrl", function(BrowseService, $scope, $window)
 {
 
     $scope.folder = [];
+    $window.onload = browseFunc('');
     $scope.browse = function (id) {
-        BrowseService.browse(id).then(function (response) {
-           $scope.folder = response.data.folder;
-        });
+        browseFunc(id);
     };
     $scope.navigate = function (asset) {
         if (asset.type == "DIR") {
-            BrowseService.browse(asset.assetId).then(function (response) {
-                $scope.folder = response.data.folder;
-            });
+            browseFunc(asset.assetId);
         }
         else {
+            // read API
           console.log("file");
         }
-
     };
-
+    function browseFunc(id) {
+        BrowseService.browse(id).then(function (response) {
+            $scope.folder = response.data.folder;
+        });
+    };
 });
 
 
--- src/main/webapp/test.jsp
+++ src/main/webapp/test.jsp
@@ -11,19 +11,25 @@
     <meta charset="UTF-8">
     <title></title>
     <script src="./resources/lib/angular-1.5.3/angular.min.js"></script>
+    <link href="./resources/css/ui.css" rel="stylesheet" />
     <script src="./app/browse/BrowseController.js" type="text/javascript"></script>
 </head>
 <body>
 
 <div ng-controller="BrowseCtrl">
     <button ng-click="browse('')">BROWSE ROOT</button>
-    <ul>
-        <li ng-repeat="asset in folder">
-            <div ng-click="navigate(asset)">
-                {{asset.name}}
-            </div>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-3 menu">
+            <ul>
+                <li ng-repeat="asset in folder">
+                    <div ng-click="navigate(asset)">
+                        {{asset.name}}
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
 </div>
 </body>
 </html>
